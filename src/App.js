e27da9fc2eebbe69f4f695644d5dcc7b099d258e
Nhu Tran <tranthiennhu1904@gmail.com>
2019-05-13 23:26:27 +0700

    create scss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import Home from './Component/Home'
 
 function App() {
   return (
     <div className="App">
-      <Home name="Nhu"></Home>
+      <Home></Home>
     </div>
   );
 }
--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Faker from 'faker';
 import _ from 'lodash';
+import './Home.scss';
 
 class Home extends React.Component {
   constructor(props) {
@@ -37,11 +38,17 @@ class Home extends React.Component {
   }
   render() {
     return (
-    <div className="container">
-      <div className="row">
-        {this.state.photos.map((photo, index) => <div key={index}><img src={photo}></img></div>)}
+      <div className="row albums-list">
+        <div className="col-md-3 albums-list-item">
+          <ion-icon className="albums-list-image" name="add"></ion-icon>
+        </div>
+        { 
+          this.state.albums.map((album, index) => 
+          <div className="col-md-3 albums-list-item" key={index}>
+            <img className="albums-list-image" src={album['photos'][0]}></img>
+          </div>)
+        }
       </div>
-    </div>
     );
   }
 
